0ef374ed271b50c350f05729b5fcd687ee093d60
CristianDv7 <103343101+CristianDv7@users.noreply.github.com>
2023-02-24 13:42:48 -0500

    Update Split.c

--- User/app/Split.c
+++ User/app/Split.c
@@ -1,8 +1,8 @@
 /*
 *********************************************************************************************************
 *
-*	模块名称 : 绿信比模块
-*	文件名称 : Split.c
+*	Nombre del módulo: módulo de relación de letras verdes
+*	Nombre del archivo : Split.c
 *	版    本 : V1.0
 *	说    明 : 
 *	修改记录 :
@@ -22,7 +22,7 @@ PhaseSplitType  RingSplit[RingMax];    //Relación de tiempo a señal verde de c
 
 
 
-//相位号 + 绿信比数据 = 绿信比相位索引
+//Número de fase + datos GSR = índice de fase GSR
 uint8_t GetSplitPhaseIndex(SplitType* Split, uint8_t PhaseNum)
 {
     uint8_t     i;
@@ -48,27 +48,27 @@ void SplitDefault(void)
     SplitTab.Maximum = SplitMax;
 
     SplitTab.Split[0].Num = 1;
-    for(i = 0; i <= 4; i++)//默认配置5个相位
+    for(i = 0; i <= 4; i++)//5 fases están configuradas por defecto
     {
         SplitTab.Split[0].Phase[i].PhaseNum = i + 1;//
-        SplitTab.Split[0].Phase[i].Time = 16;       //相位的放行时间。包含绿灯、绿闪、黄灯、全红时间
-        SplitTab.Split[0].Phase[i].Mode = SM_None;  //绿信比模式 2-无
-        SplitTab.Split[0].Phase[i].Coord = SC_FIXED;//协调配置Coord bit2：1-作为固定相位
+        SplitTab.Split[0].Phase[i].Time = 16;       //Tiempo de liberación de fase. Contiene luz verde, flash verde, luz amarilla, tiempo rojo completo
+        SplitTab.Split[0].Phase[i].Mode = SM_None;  //Relación de señal verde Modo 2-Ninguno
+        SplitTab.Split[0].Phase[i].Coord = SC_FIXED;//Configuración del coordinador Coord bit2: 1-como fase fija
     }
 }
 
-//默认的绿信比配置
-//绿信比1的默认配置
+//Configuración de proporción de letra verde predeterminada
+//La configuración predeterminada de Green Letter Ratio 1
 void SplitDataInit(uint8_t n)
 {
     uint8_t i;
     SplitTab.Split[n].Num = n + 1;
-    for(i = 0; i <= 4; i++)//默认配置5个相位
+    for(i = 0; i <= 4; i++)//5 fases están configuradas por defecto
     {
         SplitTab.Split[n].Phase[i].PhaseNum = i + 1;//
-        SplitTab.Split[n].Phase[i].Time = 18;       //相位的放行时间。包含绿灯、绿闪、黄灯、全红时间
-        SplitTab.Split[n].Phase[i].Mode = SM_None;  //绿信比模式 2-无
-        SplitTab.Split[n].Phase[i].Coord = SC_FIXED;//协调配置Coord bit2：1-作为固定相位
+        SplitTab.Split[n].Phase[i].Time = 18;       //Tiempo de liberación de fase. Contiene luz verde, flash verde, luz amarilla, tiempo rojo completo
+        SplitTab.Split[n].Phase[i].Mode = SM_None;  //Relación de señal verde Modo 2-Ninguno
+        SplitTab.Split[n].Phase[i].Coord = SC_FIXED;//Configuración del coordinador Coord bit2: 1-como fase fija
     }
 }
 
@@ -79,10 +79,10 @@ void SplitXDataInit(SplitType* Split)
     for(n=0;n<=4;n++)
     {
         Split->Phase[n].PhaseNum = n + 1;//
-        Split->Phase[n].Time = 18;      //相位的放行时间。包含了机动车相位的绿灯、绿闪、黄灯、全红时间
-                                            //以及行人相位的放行时间和清空时间。
-        Split->Phase[n].Mode = SM_None;  //绿信比模式 2-无
-        Split->Phase[n].Coord = SC_FIXED;//协调配置Coord bit2：1-作为固定相位
+        Split->Phase[n].Time = 18;      //Tiempo de liberación de fase. Incluyendo la luz verde, el destello verde, la luz amarilla y el tiempo rojo 
+                                            //completo de la fase del vehículo de motor Y el tiempo de liberación y tiempo de despeje de la fase peatonal.
+        Split->Phase[n].Mode = SM_None;  //Letra verde que modo 2 - no
+        Split->Phase[n].Coord = SC_FIXED;//Configuración del coordinador Coord bit2: 1-como fase fija
     }
 }
 
