64cf9a98836f3828ec87c7ea5129dadf7093dce2
CristianDv7 <103343101+CristianDv7@users.noreply.github.com>
2023-03-10 12:43:13 -0500

    Update can_network.c

--- User/bsp/src/can_network.c
+++ User/bsp/src/can_network.c
@@ -1,26 +1,26 @@
 /*
 *********************************************************************************************************
 *	                                  
-*	模块名称 : CAN网络程序。
-*	文件名称 : can_network.c
-*	版    本 : V1.0
+*	Nombre del m贸dulo: programa de red CAN.
+*	Nombre del archivo: can_network.c
+*	Versi贸n: V1.0
 *
 *********************************************************************************************************
 */
 #include "bsp.h"
 
 
-/* 定义全局变量 */
-CanTxMsg CanTxMsgStruct;			/* 用于发送 */
-CanRxMsg CanRxMsgStruct;			/* 用于接收 */
+/* definir variables globales */
+CanTxMsg CanTxMsgStruct;			/* para enviar */
+CanRxMsg CanRxMsgStruct;			/* para recibir */
 
 
 /*
 *********************************************************************************************************
-*	函 数 名: SendCanMsg
-*	功能说明: 发送一包数据
-*	形    参：无
-*	返 回 值: 无
+*	Nombre de la funci贸n: SendCanMsg
+*	Descripci贸n de la funci贸n: enviar un paquete de datos
+*	Par谩metros formales: ninguno
+*	Valor devuelto: Ninguno
 *********************************************************************************************************
 */
 void SendCanMsg(uint8_t *p, uint8_t length)
@@ -37,10 +37,10 @@ void SendCanMsg(uint8_t *p, uint8_t length)
 
 /*
 *********************************************************************************************************
-*	函 数 名: can_Init
-*	功能说明: 配置CAN硬件
-*	形    参：无
-*	返 回 值: 无
+*	Nombre de la funci贸n: can_Init
+*	Descripci贸n de la funci贸n: configurar el hardware CAN
+*	Par谩metros formales: ninguno
+*	Valor devuelto: Ninguno
 *********************************************************************************************************
 */
 void can_Init(void)
@@ -57,31 +57,31 @@ void can_Init(void)
     uint32_t ext_id = 0;
     uint32_t mask = 0;
     
-	/* PA11，PA12口线设置为AFIO模式, 切换到CAN功能 */
+	/* Las l铆neas de puerto PA11, PA12 est谩n configuradas en modo AFIO, cambie a la funci贸n CAN */
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 	
-	/* 使能GPIO时钟 */
+	/* Habilitar reloj GPIO */
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 	
-	/* 配置CAN信号接收引脚: RX */
+	/* Configurar pin de recepci贸n de se帽al CAN: RX */
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;	/* GPIO配置为上拉输入模式 */
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;	/* GPIO configurado como modo de entrada pull-up */
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 	
-	/* 配置CAN信号发送引脚: TX */
+	/* Configurar pin de env铆o de se帽al CAN: TX */
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;		/* 配置为复用推挽输出 */
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	/* 设置GPIO最大速度 */
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;		/* Configurar como salida push-pull multiplexada */
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	/* Establecer la velocidad m谩xima de GPIO */
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 	
-	/* 原生的CAN引脚和USB引脚是相同的口线，引脚的重映射功能将CAN引脚切换到PB8，PB9 */
-	GPIO_PinRemapConfig(GPIO_Remap1_CAN1 , ENABLE);	/* 使能CAN1的重映射 */
+	/* El pin CAN original y el pin USB son la misma l铆nea de puerto, la funci贸n de reasignaci贸n de pin cambia el pin CAN a PB8, PB9 */
+	GPIO_PinRemapConfig(GPIO_Remap1_CAN1 , ENABLE);	/* Habilitar la reasignaci贸n de CAN1 */
 	
-	/* 使能CAN外设时钟 */
+	/* Habilitar reloj perif茅rico CAN */
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1, ENABLE);
 	
-	CAN_DeInit(CAN1);											/* 复位CAN寄存器 */
-	CAN_StructInit(&CAN_InitStructure);		/* 填充CAN参数结构体成员为缺省值 */
+	CAN_DeInit(CAN1);				/* Restablecer registros CAN */
+	CAN_StructInit(&CAN_InitStructure);		/* Rellenar los miembros de la estructura de par谩metros CAN con valores predeterminados */
 	
 	/*
 		TTCM = time triggered communication mode
@@ -91,22 +91,22 @@ void can_Init(void)
 		RFLM = receive FIFO locked mode 
 		TXFP = transmit FIFO priority		
 	*/
-	CAN_InitStructure.CAN_TTCM = DISABLE;			/* 禁止时间触发模式（不生成时间戳), T  */
-	CAN_InitStructure.CAN_ABOM = DISABLE;			/* 禁止自动总线关闭管理 */
-	CAN_InitStructure.CAN_AWUM = DISABLE;			/* 禁止自动唤醒模式 */
-	CAN_InitStructure.CAN_NART = DISABLE;			/* 禁止仲裁丢失或出错后的自动重传功能 */
-	CAN_InitStructure.CAN_RFLM = DISABLE;			/* 禁止接收FIFO加锁模式 */
-	CAN_InitStructure.CAN_TXFP = DISABLE;			/* 禁止传输FIFO优先级 */
-	CAN_InitStructure.CAN_Mode = CAN_Mode_Normal;	/* 设置CAN为正常工作模式 */
+	CAN_InitStructure.CAN_TTCM = DISABLE;			/* Deshabilitar el modo activado por tiempo (no generar marcas de tiempo), T */
+	CAN_InitStructure.CAN_ABOM = DISABLE;			/* deshabilitar la gesti贸n de apagado autom谩tico de bus */
+	CAN_InitStructure.CAN_AWUM = DISABLE;			/* Deshabilitar el modo de activaci贸n autom谩tica */
+	CAN_InitStructure.CAN_NART = DISABLE;			/* Deshabilitar la retransmisi贸n autom谩tica despu茅s de una p茅rdida o error de arbitraje */
+	CAN_InitStructure.CAN_RFLM = DISABLE;			/* Deshabilitar el modo de bloqueo FIFO de recepci贸n */
+	CAN_InitStructure.CAN_TXFP = DISABLE;			/* Deshabilitar la prioridad FIFO de transmisi贸n */
+	CAN_InitStructure.CAN_Mode = CAN_Mode_Normal;	/* Establecer CAN en modo de trabajo normal */
 	
 	/* 
-		CAN 波特率 = RCC_APB1Periph_CAN / Prescaler / (SJW + BS1 + BS2);
+		Tasa de baudios CAN = RCC_APB1Periph_CAN / Prescaler / (SJW + BS1 + BS2);
 		
 		SJW = synchronisation_jump_width 
 		BS = bit_segment
 		
-		本例中，设置CAN波特率为500Kbps		
-		CAN 波特率 = 360000000 / 6 / (1 + 6 + 5) / = 500kHz		
+		En este ejemplo, establezca la tasa de baudios de CAN en 500 Kbps		
+		Tasa de baudios CAN = 360000000 / 6 / (1 + 6 + 5) / = 500kHz		
 	*/
 	CAN_InitStructure.CAN_SJW = CAN_SJW_1tq;
 	CAN_InitStructure.CAN_BS1 = CAN_BS1_8tq;
@@ -114,51 +114,51 @@ void can_Init(void)
 	CAN_InitStructure.CAN_Prescaler = 9;
 	CAN_Init(CAN1, &CAN_InitStructure);
 	
-	/* 设置CAN滤波器0 */
-	CAN_FilterInitStructure.CAN_FilterNumber = 0;		/* 滤波器序号，0-13，共14个滤波器 */
-	CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask;		/* 滤波器模式，设置ID掩码模式 */
-	CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;	/* 32位滤波 */
-    //标准帧
-	CAN_FilterInitStructure.CAN_FilterIdHigh = std_id<<5;               /* 掩码后ID的高16bit */
-	CAN_FilterInitStructure.CAN_FilterIdLow = 0 | CAN_ID_STD;			/* 掩码后ID的低16bit */
-    //拓展帧
-	//CAN_FilterInitStructure.CAN_FilterIdHigh = ((ext_id<<3)>>16)&0xffff;/* 掩码后ID的高16bit */
-	//CAN_FilterInitStructure.CAN_FilterIdLow = (ext_id<<3) | CAN_ID_EXT; /* 掩码后ID的低16bit */
+	/* Establecer filtro CAN 0 */
+	CAN_FilterInitStructure.CAN_FilterNumber = 0;		/* N煤mero de filtro, 0-13, 14 filtros en total */
+	CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask;		/* modo de filtro, establece el modo de m谩scara de ID */
+	CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;	/* filtro de 32 bits */
+    //Marco est谩ndar
+	CAN_FilterInitStructure.CAN_FilterIdHigh = std_id<<5;               /* Alto 16 bits de ID despu茅s de la m谩scara */
+	CAN_FilterInitStructure.CAN_FilterIdLow = 0 | CAN_ID_STD;			/* Los 16 bits inferiores del ID despu茅s de la m谩scara */
+    //Marco extendido
+	//CAN_FilterInitStructure.CAN_FilterIdHigh = ((ext_id<<3)>>16)&0xffff;/* Alto 16 bits de ID despu茅s de la m谩scara */
+	//CAN_FilterInitStructure.CAN_FilterIdLow = (ext_id<<3) | CAN_ID_EXT; /* ID bajo de 16 bits despu茅s de la m谩scara */
     
-    //屏蔽寄存器的设置
-	//这里的思路是先将标准CAN ID和扩展CAN ID对应的ID值先异或后取反，为什么？
-    //异或是为了找出两个CAN ID有哪些位是相同的，是相同的位则说明需要关心，
-    //需要关心的位对应的屏蔽码位应该设置为1，因此需要取反一下。最后再整体左移3位。
+    // Ajustes de registro de m谩scara
+	//La idea aqu铆 es primero XOR los valores de ID correspondientes al CAN ID est谩ndar y al CAN ID extendido, y luego invertirlos, 驴por qu茅?
+    //XOR es para averiguar qu茅 bits de los dos CAN ID son iguales, y si son iguales, significa que debemos preocuparnos.
+    //El bit del c贸digo de m谩scara correspondiente al bit que debe preocuparse debe establecerse en 1, por lo que debe invertirse. Finalmente, se desplaza 3 bits a la izquierda en su conjunto.
     
-    //这里为什么左移18位？因为从ISO11898中可以看出，
-    //标准CAN ID占ID18~ID28，为了与CAN_FilterIdHigh对齐，应左移2位，
-    //接着为了与扩展CAN对应，还应该再左移16位，因此，总共应左移2+16＝18位。
-    //也可以用另一个方式来理解：直接看Mapping的内容，发现STDID相对EXID[0]偏移了18位,因此左移18位.
+    //驴Por qu茅 te desplazas 18 bits a la izquierda aqu铆? Porque se puede ver desde ISO11898,
+    //El CAN ID est谩ndar ocupa ID18~ID28, para alinearse con CAN_FilterIdHigh, debe desplazarse a la izquierda 2 bits,
+    //Entonces, para corresponder al CAN extendido, se deben desplazar otros 16 bits a la izquierda, por lo tanto, se debe desplazar a la izquierda un total de 2+16=18 bits.
+    //Tambi茅n se puede entender de otra manera: mire directamente el contenido de Mapping y descubra que STDID est谩 compensado por 18 bits en relaci贸n con EXID[0], por lo que est谩 desplazado hacia la izquierda por 18 bits.
     
 	mask =(std_id<<18);
-	mask ^=ext_id;//将对齐后的标准CAN与扩展CAN异或后取反
+	mask ^=ext_id;//XOR el CAN est谩ndar alineado y el CAN extendido y luego invertir
 	mask =~mask;
-	mask <<=3;//再整体左移3位
-	mask |=0x02; //只接收数据帧，不接收远程帧
-	CAN_FilterInitStructure.CAN_FilterMaskIdHigh=(mask>>16)&0xffff; //设置屏蔽寄存器高字节
-	CAN_FilterInitStructure.CAN_FilterMaskIdLow=mask&0xffff;   //设置屏蔽寄存器低字节
+	mask <<=3;//Luego desplace a la izquierda por 3 bits como un todo
+	mask |=0x02; //Recibir solo tramas de datos, no tramas remotas
+	CAN_FilterInitStructure.CAN_FilterMaskIdHigh=(mask>>16)&0xffff; //Establecer el byte alto del registro de m谩scara
+	CAN_FilterInitStructure.CAN_FilterMaskIdLow=mask&0xffff;   //Establecer el byte bajo del registro de m谩scara
     
-	CAN_FilterInitStructure.CAN_FilterFIFOAssignment = CAN_FIFO0;		/* 滤波器绑定FIFO 0 */
-	CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;				/* 使能滤波器 */
+	CAN_FilterInitStructure.CAN_FilterFIFOAssignment = CAN_FIFO0;		/* enlace de filtro FIFO 0 */
+	CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;				/* habilitar filtro */
 	CAN_FilterInit(&CAN_FilterInitStructure);
-    /* 填充发送参数， 也可以再每次发送的时候填 */
+    /* Rellene los par谩metros de env铆o, tambi茅n puede rellenar cada vez que env铆e */
 	CanTxMsgStruct.StdId = 0x29;
 	CanTxMsgStruct.ExtId = 0x00;
-	CanTxMsgStruct.RTR = CAN_RTR_DATA;	//数据帧-远程帧
-	CanTxMsgStruct.IDE = CAN_ID_STD;	//CAN_ID_EXD;		//标准帧-拓展帧
+	CanTxMsgStruct.RTR = CAN_RTR_DATA;	//marco de datos - marco remoto
+	CanTxMsgStruct.IDE = CAN_ID_STD;	//CAN_ID_EXD;		//marco est谩ndar-marco extendido
 }
 
 /*
 *********************************************************************************************************
-*	函 数 名: can_NVIC_Config
-*	功能说明: 配置CAN中断
-*	形    参：无
-*	返 回 值: 无
+*	Nombre de la funci贸n: can_NVIC_Config
+*	Descripci贸n de la funci贸n: Configurar interrupci贸n CAN
+*	Par谩metros formales: ninguno
+*	Valor devuelto: Ninguno
 *********************************************************************************************************
 */  
 void can_NVIC_Config(void)
@@ -173,16 +173,16 @@ void can_NVIC_Config(void)
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 	
-	/* CAN FIFO0 消息接收中断使能 */ 
+	/* Habilitaci贸n de interrupci贸n de recepci贸n de mensaje CAN FIFO0 */ 
 	CAN_ITConfig(CAN1, CAN_IT_FMP0, ENABLE);
 }
 
 /*
 *********************************************************************************************************
-*	函 数 名: can_ISR
-*	功能说明: CAN中断服务程序. 这个函数在 stm32f10x_it.c中被调用
-*	形    参：无
-*	返 回 值: 无
+*	Nombre de la funci贸n: can_ISR
+*	Descripci贸n de la funci贸n: Rutina de servicio de interrupci贸n CAN. Esta funci贸n se llama en stm32f10x_it.c
+*	Par谩metros formales: ninguno
+*	Valor devuelto: Ninguno
 *********************************************************************************************************
 */ 
 void can_ISR(void)
@@ -199,7 +199,7 @@ void can_ISR(void)
             CAN_Transmit(CAN1, &CanTxMsgStruct);
             //SendCanMsg(CanTxMsgStruct.Data, 6);
             
-            if(Manual_start == CanRxMsgStruct.Data[0])//开始手动
+            if(Manual_start == CanRxMsgStruct.Data[0])//empezar manualmente
             {
                 #if DEBUG
                 printf("RemoteGuard\r\n");
