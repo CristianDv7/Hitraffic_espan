c49d58b222ed5145445b103a1730698089be590b
CristianDv7 <103343101+CristianDv7@users.noreply.github.com>
2023-03-10 10:30:09 -0500

    Update bsp_oled.c

--- User/bsp/src/bsp_oled.c
+++ User/bsp/src/bsp_oled.c
@@ -1,8 +1,8 @@
 #include "bsp.h"
 #include "oled_font.h" 
 
-#define OLED_CMD    0	//写命令
-#define OLED_DATA   1	//写数据
+#define OLED_CMD    0	//comando de escritura
+#define OLED_DATA   1	//escribir datos
 
 #define SIZE 		16
 #define XLevelL		0x00
@@ -13,13 +13,12 @@
 #define X_WIDTH 	128
 #define Y_WIDTH 	64	  
 
-/*
-SPI2 用于OLED显示屏,实际程序使用模拟SPI
-	OLED CS			PE15  片选
-	OLED RES		PB12 复位
+/*SPI2 se usa para la pantalla OLED, el programa real usa SPI simulado
+	OLED CS			Selecci贸n de chip PE15
+	OLED RES		Restablecer PB12
 	OLED SCK		PB13 
-	OLED DC 		PB14 数据&命令 控制
-	OLED DIN		PB15 数据
+	OLED DC 		Control de datos y comandos PB14
+	OLED DIN		datos PB15
 */
 #define OLED_CS_PIN		GPIO_Pin_15
 
@@ -47,21 +46,21 @@ SPI2 用于OLED显示屏,实际程序使用模拟SPI
 
 /*
 *********************************************************************************************************
-*	函 数 名: bsp_Init_OLED_gpio
-*	功能说明: 
-*	形    参: 无
-*	返 回 值: 无
+*	Nombre de la funci贸n: bsp_Init_OLED_gpio
+*	Funci贸n descriptiva: 
+*	Par谩metros formales: Ninguno
+*	Valor devuelto: Ninguno
 *********************************************************************************************************
 */
 void bsp_Init_OLED_gpio(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 
-	/* 打开GPIO时钟 */
+	/* 鎵撳紑GPIO鏃堕挓 */
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOB , ENABLE);
 
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;		/* 设为输出口 */
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	  /* IO口最大速度 */
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;		/* establecer como puerto de salida */
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	 /* Velocidad m谩xima del puerto IO */
 
 	GPIO_InitStructure.GPIO_Pin = OLED_CS_PIN;	//E15
 	GPIO_Init(GPIOE, &GPIO_InitStructure);
@@ -73,7 +72,7 @@ void bsp_Init_OLED_gpio(void)
 static void OLED_WriteCmd(uint8_t _ucCmd)
 {
 	uint8_t i;
-	OLED_DC_Clr(); //命令
+	OLED_DC_Clr(); //Orden
 	OLED_CS_Clr();
 	for (i = 0; i < 8; i++)
 	{
@@ -97,7 +96,7 @@ static void OLED_WriteCmd(uint8_t _ucCmd)
 static void OLED_WriteData(uint8_t _ucData)
 {
 	uint8_t i;
-	OLED_DC_Set(); //数据
+	OLED_DC_Set(); //datos
 	OLED_CS_Clr();
 	for (i = 0; i < 8; i++)
 	{
@@ -152,29 +151,29 @@ void OLED_Set_Pos(uint8_t x, uint8_t y)
 	OLED_WriteCmd(((x&0xf0)>>4)|0x10);
 	OLED_WriteCmd((x&0x0f)|0x01); 
 }
-//开启OLED显示    
+//Habilitar pantalla OLED    
 void OLED_Display_On(void)
 {
-	OLED_WriteCmd(0X8D);  //SET DCDC命令
+	OLED_WriteCmd(0X8D);  //CONFIGURAR comando DCDC
 	OLED_WriteCmd(0X14);  //DCDC ON
 	OLED_WriteCmd(0XAF);  //DISPLAY ON
 }
-//关闭OLED显示     
+//Apaga la pantalla OLED   
 void OLED_Display_Off(void)
 {
-	OLED_WriteCmd(0X8D);  //SET DCDC命令
+	OLED_WriteCmd(0X8D);  ////CONFIGURAR comando DCDC
 	OLED_WriteCmd(0X10);  //DCDC OFF
 	OLED_WriteCmd(0XAE);  //DISPLAY OFF
 }		   			 
-//清屏函数,清完屏,整个屏幕是黑色的!和没点亮一样!!!	  
+// Funci贸n de pantalla clara, despu茅s de borrar la pantalla, 隆toda la pantalla est谩 negra! 隆Es lo mismo que no encenderse!  
 void OLED_Clear(void)  
 {  
 	uint8_t i,n;		    
 	for(i=0;i<8;i++)  
 	{  
-		OLED_WriteCmd (0xb0+i);    //设置页地址（0~7）
-		OLED_WriteCmd (0x00);      //设置显示位置—列低地址
-		OLED_WriteCmd (0x10);      //设置显示位置—列高地址   
+		OLED_WriteCmd (0xb0+i);    //Configuraci贸n de la direcci贸n de la p谩gina (0~7)
+		OLED_WriteCmd (0x00);      //Establecer posici贸n de visualizaci贸n: direcci贸n de columna baja
+		OLED_WriteCmd (0x10);      //Establecer posici贸n de visualizaci贸n: direcci贸n de altura de columna
 		for(n=0;n<128;n++)OLED_WriteData(0); 
 	}
 }
@@ -187,15 +186,15 @@ void oled_Init(void)
     bsp_DelayMS(200);
 	OLED_RES_Set();
 	
-	OLED_WriteCmd(0xAE);//--turn off oled panel
-	OLED_WriteCmd(0x00);//---set low column address
-	OLED_WriteCmd(0x10);//---set high column address
-	OLED_WriteCmd(0x40);//--set start line address  Set Mapping RAM Display Start Line (0x00~0x3F)
-	OLED_WriteCmd(0x81);//--set contrast control register
-	OLED_WriteCmd(0xCF); // Set SEG Output Current Brightness
-	OLED_WriteCmd(0xA1);//--Set SEG/Column Mapping     0xa0左右反置 0xa1正常
-	OLED_WriteCmd(0xC8);//Set COM/Row Scan Direction   0xc0上下反置 0xc8正常
-	OLED_WriteCmd(0xA6);//--set normal display
+	OLED_WriteCmd(0xAE);//--apague el panel oled
+	OLED_WriteCmd(0x00);//---establecer direcci贸n de columna baja
+	OLED_WriteCmd(0x10);//---establecer direcci贸n de columna alta
+	OLED_WriteCmd(0x40);//--establecer la direcci贸n de la l铆nea de inicio Establecer asignaci贸n RAM Mostrar l铆nea de inicio (0x00~0x3F)
+	OLED_WriteCmd(0x81);//--establecer registro de control de contraste
+	OLED_WriteCmd(0xCF); //Establecer el brillo actual de salida SEG
+	OLED_WriteCmd(0xA1);//--Establecer SEG/Asignaci贸n de columnas 0x0 izquierda y derecha 0x1 normal
+	OLED_WriteCmd(0xC8);//Establecer direcci贸n de escaneo COM/fila 0xc0 inversa arriba y abajo 0xc8 normal
+	OLED_WriteCmd(0xA6);//--establecer visualizaci贸n normal
 	OLED_WriteCmd(0xA8);//--set multiplex ratio(1 to 64)
 	OLED_WriteCmd(0x3f);//--1/64 duty
 	OLED_WriteCmd(0xD3);//-set display offset	Shift Mapping RAM Counter (0x00~0x3F)
@@ -221,15 +220,15 @@ void oled_Init(void)
 	OLED_Set_Pos(0,0); 
 }
 
-//在指定位置显示一个字符,包括部分字符
+//Mostrar un car谩cter en la posici贸n especificada, incluidos algunos caracteres
 //x:0~127
 //y:0~63
-//mode:0,反白显示;1,正常显示				 
-//size:选择字体 16/12 
+//modo:0, se muestra al rev茅s; 1, se muestra normalmente				 
+//tama帽o: seleccionar fuente 16/12 
 void OLED_ShowChar(uint8_t x, uint8_t y, uint8_t chr)
 {
 	uint8_t c=0, i=0;	
-    c = chr - ' ';//得到偏移后的值 
+    c = chr - ' ';// obtener el valor de compensaci贸n
     if(x > Max_Column-1){x=0;y=y+2;}
     if(SIZE ==16)
     {
@@ -248,7 +247,7 @@ void OLED_ShowChar(uint8_t x, uint8_t y, uint8_t chr)
     }
 }
 
-//显示一个字符号串
+//Mostrar una cadena de caracteres
 void OLED_ShowString(uint8_t x, uint8_t y, char *chr)
 {
 	uint8_t j=0;
